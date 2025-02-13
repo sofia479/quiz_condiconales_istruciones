e586504edef94a577344389037aea8ccde05d9dd
sofia479 <bertagalvis@coleigioguanenta.edu.co>
2025-02-13 16:15:10 -0500

    asd

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# quiz_condiconales_istruciones
+
+# Quiz instrucciones condicionales
+
+### Realice el análisis, diseño y construcción para resolver el siguiente problema:
+
+Dados tres números a, b y c, correspondientes a la longitud de los lados de una figura geométrica, determinar si pueden formar los lados de un triángulo.
+
+# Analisis
+## input
+a,b,c
+
+## processing
+si la suma de dos valores es mayor al otro valor es posible formar un triangulo,de lo contrario no
+
+## output
+
+r (es posibl/no es posible)
+
+
+## Diseño
+
+### Diagrama de flujo
+![Diagrama de flujo](diagrama.png)"Diagrama de flujo"
+
+# consustruccion
+
---
+++ quiz.py
@@ -0,0 +1,34 @@
+# Quiz instrucciones condicionales
+"""Dados tres números a, b y c, correspondientes a la longitud de los lados de una figura geométrica, determinar si pueden formar los lados de un triángulo."""
+
+print("------------------------------")
+print("----- VERIFICAR TRIANGULO ----")
+print("------------------------------")
+
+# input
+a=int(input("ingrese valor:"))
+
+b=int(input("ingrese valor:"))
+
+c=int(input("ingrese el valor"))
+
+
+# processing
+
+if a+b>c:
+    r=("es posible")
+
+if a+c>b
+    r=("es posible")
+
+if b+c>a:
+    r=(" no es posible")
+
+else
+    r=("no es posible")
+
+
+    # output
+
+
+    print("resultado:" +str(r))
